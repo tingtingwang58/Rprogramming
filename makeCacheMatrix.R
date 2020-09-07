5b7a87fa299be8a19b53b741c72193e2e5c95b37
tingtingwang58 <68458948+tingtingwang58@users.noreply.github.com>
2020-09-07 15:43:29 +0800

    Code file

---
+++ makeCacheMatrix.R
@@ -0,0 +1,27 @@
+## this MakeCacheMatrix function creates a special “matrix” object
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL 
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse<- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+## Cachemean function computes the inverse of the special “matrix”. Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message('getting cached data')
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
+
